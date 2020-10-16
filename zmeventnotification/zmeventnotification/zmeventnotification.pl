78dffd2322b0b9b7182b31b690905389f038f691
dlandon <dlandon@familyoflandon.com>
2020-10-16 05:14:54 -0400

    Update to ES 6.

--- zmeventnotification/zmeventnotification/zmeventnotification.pl
+++ zmeventnotification/zmeventnotification/zmeventnotification.pl
@@ -43,7 +43,7 @@ use Symbol qw(qualify_to_ref);
 use IO::Select;
 
 ####################################
-my $app_version = '6.0.2-Docker';
+my $app_version = '6.0.2';
 ####################################
 
 # do this before any log init etc.
